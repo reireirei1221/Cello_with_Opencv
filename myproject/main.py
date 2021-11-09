5373a7b37a145818268bdedb54aa3e3d37c508f0
reireirei1221 <reireirei1221@icloud.com>
2021-11-09 17:09:06 +0900

    right or left distingushed

--- myproject/main.py
+++ myproject/main.py
@@ -15,6 +15,8 @@ bow_length = 1000
 
 string = None
 
+cnt = 0
+
 def find_which_string(x, y):
 	x_ = x-cx_cello
 	y_ = -(y-cy_cello)
@@ -29,8 +31,6 @@ def find_which_string(x, y):
 	else:
 		return 4
 	
-		
-
 while True:
 	success, img = cap.read()
 	img = cv2.flip(img, 1)
@@ -39,32 +39,42 @@ while True:
  
 	if results.multi_hand_landmarks:
 		for handLms in results.multi_hand_landmarks:
-			for id, lm in enumerate(handLms.landmark):
+			h, w, c = img.shape
+
+			rightHand = (cx_cello < handLms.landmark[5].x * w)
+
+			# for id, lm in enumerate(handLms.landmark):
 				# print(id)
 				# print(lm)
-				h, w, c = img.shape
-				cx, cy = int(lm.x * w), int(lm.y * h)
-				if id == 5:
-					cx_5, cy_5 = cx, cy
-				if id == 17:
-					cx_17, cy_17 = cx, cy
+				# h, w, c = img.shape
+				# cx, cy = int(lm.x * w), int(lm.y * h)
+				# if id == 5:
+					# cx_5, cy_5 = cx, cy
+				# if id == 17:
+					# cx_17, cy_17 = cx, cy
 			
 			mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
-			cx_hand = int((cx_5+cx_17)/2)
-			cy_hand = int((cy_5+cy_17)/2)
+			if rightHand:
+				cx_5 = handLms.landmark[7].x * w
+				cy_5 = handLms.landmark[7].y * h
+				cx_17 = handLms.landmark[19].x * w
+				cy_17 = handLms.landmark[19].y * h
 
-			distance = ((cx_hand-cx_cello)**2 + (cy_hand-cy_cello)**2)**0.5
+				cx_hand = int((cx_5+cx_17)/2)
+				cy_hand = int((cy_5+cy_17)/2)
 
-			cx_bow = int((bow_length*cx_cello + (distance-bow_length)*cx_hand)/distance)
-			cy_bow = int((bow_length*cy_cello + (distance-bow_length)*cy_hand)/distance)
+				distance = ((cx_hand-cx_cello)**2 + (cy_hand-cy_cello)**2)**0.5
 
-			img = cv2.line(img, (cx_hand, cy_hand), (cx_bow, cy_bow), (255, 255, 255), 10)
+				cx_bow = int((bow_length*cx_cello + (distance-bow_length)*cx_hand)/distance)
+				cy_bow = int((bow_length*cy_cello + (distance-bow_length)*cy_hand)/distance)
 
-			if string != find_which_string(cx_hand, cy_hand):
-				string = find_which_string(cx_hand, cy_hand)
-				subprocess.Popen(['python3', 'sub.py', 'piano'])
-				print(string)
+				img = cv2.line(img, (cx_hand, cy_hand), (cx_bow, cy_bow), (255, 255, 255), 10)
+
+				if string != find_which_string(cx_hand, cy_hand):
+					string = find_which_string(cx_hand, cy_hand)
+					subprocess.Popen(['python3', 'sub.py', 'piano'])
+					print(string)
 
 	img = cv2.line(img, (cx_cello+500, cy_cello-200), (cx_cello-500, cy_cello+200), (255, 255, 255), 5)
 
